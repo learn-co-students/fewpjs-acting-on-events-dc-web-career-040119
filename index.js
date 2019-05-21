753c33e88366e4a9c863883b8a9f19bc567d31ad
NosaPrecious <Okundaye.preciousnosa@gmail.com>
2019-05-20 23:11:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+var dodger = document.getElementById('dodger');
+
+
+function moveDodgerLeft(){
+  var leftNums = dodger.style.left.replace('px', '');
+    var leftSide = parseInt(leftNums, 10)
+    if (leftSide > 0) {
+      dodger.style.left = `${leftSide - 1}px`
+    }
+}
+
+function moveDodgerRight(){
+  var leftNums = dodger.style.left.replace('px', '');
+  var leftSide = parseInt(leftNums, 10)
+  if (leftSide > 0) {
+    dodger.style.left = `${leftSide + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
